e586efba03f9791486443fcefd68f7a57f3cf6d0
niyer20 <nakulpiyer@gmail.com>
2018-05-26 15:17:05 -0400

    Newest With All the Images
    Newest With All the Images (missing descriptions??)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();
-        var divam = -1.7;
+        var divam = -2;
         $("#banner").css("top", scrollTop/divam+"px");
         $("#banner").css("height", 10000-(Math.round(scrollTop/divam))+"px");
         if (scrollTop > 350) {
@@ -27,7 +27,7 @@ $(document).ready(function(){
     }
 
     var foods = ["ff", "pz", "fc", "ic", "nc"];
-    var foodlinks = ["http://www.clker.com/cliparts/E/X/A/Q/E/f/french-fries-hi.png", "https://content.mycutegraphics.com/graphics/food/whole-pepperoni-pizza.png", "http://images.clipartpanda.com/fried-chicken-clipart-fried-chicken-clipart.png", "https://content.mycutegraphics.com/graphics/icecream/ice-cream-clip-art-banana-split-clip-art.png", "https://openclipart.org/download/274956/nachos.svg"];
+    var foodlinks = ["http://www.clker.com/cliparts/E/X/A/Q/E/f/french-fries-hi.png", "https://content.mycutegraphics.com/graphics/food/whole-pepperoni-pizza.png", "http://images.clipartpanda.com/fried-chicken-clipart-fried-chicken-clipart.png", "https://content.mycutegraphics.com/graphics/icecream/ice-cream-clip-art-banana-split-clip-art.png", "http://www.clker.com/cliparts/Z/9/L/L/4/J/hungry-nachos-hi.png"];
 
     /*for(var f = 0; f < 5; f++) {
       for(var r = 0; r < 3; r++) {
@@ -58,35 +58,63 @@ $(document).ready(function(){
         $("#" + $(this).attr('id')+"b").css("visibility", "hidden");
     });
 
+
+
+
+
+
+
     $("#" + foods[0]).hover(function() {
       $(".foods").css("visibility", "hidden");
       $("." + foods[0] + "s").css("visibility", "visible");
+      $("#img1").attr("src", "http://tastykitchen.com/recipes/wp-content/uploads/sites/2/2014/07/Extra-Crispy-Oven-Baked-French-Fries-by-Jen-and-Emily-@-Layers-of-Happiness-on-July-2-2014-410x273.jpg");
+      $("#img2").attr("src", "https://i.ytimg.com/vi/AhImW2R9VDg/maxresdefault.jpg");
+      $("#img3").attr("src", "https://www.pizzahagerstown.com/wp-content/uploads/lunch-dinner_555-menu_crispy-chicken-sandwich-fries.jpg");
       }, function() {
     });
 
     $("#" + foods[1]).hover(function() {
       $(".foods").css("visibility", "hidden");
       $("." + foods[1] + "s").css("visibility", "visible");
+      $("#img1").attr("src", "https://media-cdn.tripadvisor.com/media/photo-s/02/b2/a3/5c/that-pizza-place.jpg");
+      $("#img2").attr("src", "https://www.360bistrobar.com/wp-content/uploads/sites/16/2017/03/veggie-pizza-600x400.jpg");
+      $("#img3").attr("src", "http://assets.kraftfoods.com/recipe_images/opendeploy/179732_640x428.jpg");
       }, function() {
     });
 
     $("#" + foods[2]).hover(function() {
       $(".foods").css("visibility", "hidden");
       $("." + foods[2] + "s").css("visibility", "visible");
+      $("#img1").attr("src", "https://assets.epicurious.com/photos/57a8a45db10b4fb03f234f34/2:1/w_1260%2Ch_630/southern-fried-chicken.jpg");
+      $("#img2").attr("src", "https://images-gmi-pmc.edge-generalmills.com/6fbc6859-e2b1-499d-b0fa-ada600c9cc3f.jpg");
+      $("#img3").attr("src", "https://www.maangchi.com/wp-content/uploads/2016/02/kkanpunggi-1.jpg");
       }, function() {
     });
 
     $("#" + foods[3]).hover(function() {
       $(".foods").css("visibility", "hidden");
       $("." + foods[3] + "s").css("visibility", "visible");
+      $("#img1").attr("src", "https://truffle-assets.imgix.net/5f5c384a-111-icecreamconecupcakes-dishland2.jpg");
+      $("#img2").attr("src", "https://cdn2.recipestonourish.com/wp-content/uploads/2011/08/Honey-Vanilla-Ice-Cream-Recipes-to-Nourish-2.jpg");
+      $("#img3").attr("src", "https://www.foodmatters.com/media/images/articles/choc-chip-icecream.jpg");
       }, function() {
     });
 
     $("#" + foods[4]).hover(function() {
       $(".foods").css("visibility", "hidden");
       $("." + foods[4] + "s").css("visibility", "visible");
+      $("#img1").attr("src", "https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/recipe_photos/28062_basic_cheese_nachos.jpg");
+      $("#img2").attr("src", "https://images-gmi-pmc.edge-generalmills.com/4f5b624d-c9d0-4a75-acfe-fadbe40a7d1e.jpg");
+      $("#img3").attr("src", "https://daiyafoods.com/wp-content/uploads/2016/05/Cinco-de-Mayo-Nachos-670x400.png");
       }, function() {
     });
+
+
+
+
+
+
+
 
     /*for(t = 0; t < 5; t++) {
         $("#" + foods[t]).hover(function(t) {
